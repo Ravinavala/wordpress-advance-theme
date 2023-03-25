eb6522e489c6c2690c674b3deef814a5f20a96e7
Ravina Vala <vala8460@gmail.com>
2023-03-24 22:06:31 -0400

    display menu and child menu, setup custom HTML to use bootstrap style for menu

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -11,7 +11,9 @@ namespace ADVANCE_THEME\Inc;
 use ADVANCE_THEME\Inc\Traits\Singleton;
 
 class Menus {
+
     use Singleton;
+
     protected function __construct() {
         //load classes
         $this->setup_hooks();
@@ -28,4 +30,26 @@ class Menus {
             'advance-theme-footer-menu' => esc_html('Footer Menu', 'advance-theme'),
         ));
     }
+
+    public function get_menu_id($location) {
+        //get all the locations
+        $locations = get_nav_menu_locations();
+        $menu_id = $locations[$location];
+        return !empty($menu_id) ? $menu_id : "";
+    }
+
+    public function get_child_menu_items($menu_array, $parent_id) {
+
+        $child_menus = array();
+        if (!empty($menu_array) && is_array($menu_array)) {
+            foreach ($menu_array as $menu) {
+                if (intval($menu->menu_item_parent) === $parent_id) {
+                    array_push($child_menus, $menu);
+                }
+            }
+        }
+
+        return $child_menus;
+    }
+
 }
