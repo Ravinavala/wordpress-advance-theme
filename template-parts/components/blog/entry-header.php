55e159eb61fbd0c80df6d9b69127961c6f4ae23e
Ravina Vala <vala8460@gmail.com>
2023-03-27 21:06:01 -0400

    save hide page title value in database, verify user access and nonce, fetch the valule

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -4,7 +4,9 @@
  *
  * @package Advancetheme
  */
+
 $the_post_id = get_the_ID();
+$hide_title = get_post_meta($the_post_id, '_hide_page_title', true);
 ?>
 <header class="entry-header">
     <?php if (has_post_thumbnail($the_post_id)) { ?>
