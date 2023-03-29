a8761022b4a8dd1733775a0ab3f65d4d6474c907
Ravina Vala <vala8460@gmail.com>
2023-03-29 16:12:47 -0400

    setup single page

--- single.php
+++ single.php
@@ -7,9 +7,32 @@
 
 get_header();
 ?>
+<div id="primary">
+    <main id="main" class="site-main mt-5" role="main">
+        <?php if (have_posts()) :
+            ?>
+            <div class="container">
+                <?php if (is_home() && !is_front_page()) { ?>
+                    <header class="mb-5">
+                        <h1 class="page-title "> 
+                            <?php single_post_title(); ?> 
+                        </h1>
+                    </header>
+                    <?php
+                }
 
-<div>
-    <?php echo esc_html_e('Single post'); ?>
+                while (have_posts()) : the_post();
+                    get_template_part('template-parts/content');
+                endwhile;
+                ?>
+            </div>
+    </div>
+    <?php
+else:
+    get_template_part('template-parts/content-none');
+endif;
+?>
+</main>
 </div>
-
-<?php get_footer(); ?>
+<?php
+get_footer();
