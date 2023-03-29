c1700fa41fb43936ac40d720b15a34d6c63b1d62
Ravina Vala <vala8460@gmail.com>
2023-03-29 16:28:25 -0400

    Add pagination on single page, page break

--- template-parts/components/blog/entry-content.php
+++ template-parts/components/blog/entry-content.php
@@ -7,21 +7,28 @@
 ?>
 
 <section class="entry-content">
-   <?php if(is_single()) {
+    <?php
+    if (is_single()) {
         the_content(
-        sprintf(wp_kses( __('Continue Reading %s <span class="meta-nav"> &rarr; </span>', 'advance-theme'), 
-                
-                [
-                    'span' => [
-                        'class' => []
-                    ]
-                ]
-                
-                ), the_title( '<span class="screen-reader-text">', '</span>'))
+                sprintf(wp_kses(__('Continue Reading %s <span class="meta-nav"> &rarr; </span>', 'advance-theme'),
+                                [
+                                    'span' => [
+                                        'class' => []
+                                    ]
+                                ]
+                        ), the_title('<span class="screen-reader-text">', '</span>'))
         );
-   } else { 
-       advance_theme_the_excerpt(25);
-       echo advance_theme_read_more();
-   } ?>
+
+        wp_link_pages(
+                [
+                    'before' => '<div class="page-links">' . esc_html__('Pages:', 'advance-theme'),
+                    'after' => '</div>'
+                ],
+        );
+    } else {
+        advance_theme_the_excerpt(25);
+        echo advance_theme_read_more();
+    }
+    ?>
 </section>
 
